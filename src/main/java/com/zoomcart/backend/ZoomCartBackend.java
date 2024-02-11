8de6038dad6b34362f315f3e29950bab87f71167
Ameen Khan <mak_24x7@hotmail.co.uk>
2024-02-11 11:34:33 -0500

    initial commit

---
+++ src/main/java/com/zoomcart/backend/ZoomCartBackend.java
@@ -0,0 +1,13 @@
+package com.zoomcart.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoomCartBackend {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoomCartBackend.class, args);
+	}
+
+}
---
+++ src/test/java/com/zoomcart/backend/ZoomCartBackendTests.java
@@ -0,0 +1,13 @@
+package com.zoomcart.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoomCartBackendTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
